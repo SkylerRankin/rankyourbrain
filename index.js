3a947fe3baac9defe10a0513d589387731120d05
Skyler <skylerrankin@gmail.com>
2017-11-07 19:04:29 -0500

    first operation

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 javascript:(function() {
   if (!document.getElementById("solve")) {
     var solve = document.createElement('script');
-    solve.src = 'http://localhost:3000/solve.js';
+    solve.src = 'https://github.com/SkylerRankin/rankyourbrain/blob/master/solve.js';
     solve.id = 'solve';
     document.body.appendChild(solve);
   }
 })();
-
-// id = answer
-// id = beforeAnswer
--- solve.js
+++ solve.js
@@ -1,2 +1,20 @@
-alert('solved');
-console.log('solved');
+var q = document.getElementById('beforeAnswer');
+var _a = q.innerHTML.split(' ');
+var a = 'error';
+switch (_a[1]) {
+  case '*':
+    a = _a[0] * _a[2];
+    break;
+  case '+':
+    a = 0 + _a[0] + _a[2];
+    break;
+  case '-':
+    a = _a[0] - _a[2];
+    break;
+  case '/':
+    a = _a[0] / _a[2];
+    break;
+}
+document.getElementById('answer').value = a;
+document.getElementById('answer').click();
+console.log(a);
